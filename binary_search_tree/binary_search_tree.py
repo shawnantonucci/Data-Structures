6d86ea81524b1a249176802b29801c093ce25ff2
shawn <shawnantonucci2018@gmail.com>
2019-06-12 12:43:19 -0700

    answered questions and added insert

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,25 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    current = self
+    target = value
+    complete = False
+
+    while not complete:
+
+      if target < current.value:
+        if current.left:
+          current = current.left
+        else:
+          current.left = BinarySearchTree(target)
+          complete = True
+
+      if target > current.value:
+        if current.right:
+          current = current.right
+        else:
+          current.right = BinarySearchTree(target)
+          complete = True
 
   def contains(self, target):
     pass
