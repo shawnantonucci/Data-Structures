ce9b6160e07e8c4adc573546e78554f0c81d1387
shawn <shawnantonucci2018@gmail.com>
2019-06-13 11:49:45 -0700

    insert/delete

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,25 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        remove_n = self.storage[0]
+        del self.storage[0]
+        self._sift_down(0)
+        return remove_n
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        pass
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass
