f33ed11e43d26288790c01388dda845d8aa2f50d
shawn <shawnantonucci2018@gmail.com>
2019-06-13 13:13:17 -0700

    bubbleUp/shiftDown

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -19,7 +19,25 @@ class Heap:
         return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        while (index - 1) // 2 >= 0:
+
+            if self.storage[(index - 1) // 2] < self.storage[index]:
+                self.storage[index], self.storage[(
+                index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+
+            index = (index - 1) // 2
 
     def _sift_down(self, index):
-        pass
+        while index * 2 + 1 <= len(self.storage) - 1:
+
+            if index * 2 + 2 > len(self.storage) - 1:
+                maxValue = index * 2 + 1
+            elif self.storage[index * 2 + 1] > self.storage[index * 2 + 2]:
+                maxValue = index * 2 + 1
+            else:
+                maxValue = index * 2 + 2
+
+            if self.storage[maxValue] > self.storage[index]:
+                self.storage[maxValue], self.storage[index] = self.storage[index], self.storage[maxValue]
+
+            index = maxValue
